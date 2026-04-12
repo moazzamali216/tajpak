f320d45749e0e009e5951591d88c1448db7f9ba8
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2026-04-12 17:25:08 +0500

    main

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,65 @@
-//   window.addEventListener("load", function () {
-//     const preloader = document.getElementById("preloader");
+  
+  // Preloader
+  window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
 
-//     preloader.style.opacity = "0";
-//     preloader.style.transition = "opacity 0.5s ease";
-// // 
-//     setTimeout(() => {
-//       preloader.style.display = "none";
-//     }, 500);
-//   });
+    preloader.style.opacity = "0";
+    preloader.style.transition = "opacity 0.5s ease";
+// 
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 500);
+  });
+
+
+  // Navbar scroll effect
+
+  const navbar = document.getElementById("navbar");
+  const navLinks = document.querySelectorAll("#navbar ul li a");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+
+      // Navbar background
+      navbar.classList.add("bg-gray-200", "shadow-md","py-1");
+      navbar.classList.remove("bg-transparent","py-4");
+
+      // Links color
+      navLinks.forEach(link => {
+        link.classList.remove("text-gray-100");
+        link.classList.add("text-gray-700");
+      });
+
+    } else {
+
+      // Reset navbar
+      navbar.classList.add("bg-transparent","py-4");
+      navbar.classList.remove("bg-gray-200", "shadow-md","py-1");
+
+      // Reset links
+      navLinks.forEach(link => {
+        link.classList.remove("text-gray-700");
+        link.classList.add("text-gray-100");
+      });
+
+    }
+  });
+
+   const openMenu = document.getElementById("openMenu");
+  const closeMenu = document.getElementById("closeMenu");
+  const sideMenu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("overlay");
+
+  function openNav() {
+    sideMenu.classList.remove("translate-x-full");
+    overlay.classList.remove("hidden");
+  }
+
+  function closeNav() {
+    sideMenu.classList.add("translate-x-full");
+    overlay.classList.add("hidden");
+  }
+
+  openMenu.addEventListener("click", openNav);
+  closeMenu.addEventListener("click", closeNav);
+  overlay.addEventListener("click", closeNav);
