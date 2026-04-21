4ae3d7863346338f78b9d1df0e35d89b759bc2cc
moazzamali216 <113228966+moazzamali216@users.noreply.github.com>
2026-04-21 16:03:03 +0500

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./**/*.html","./categories/**/*.html","./public/**/*.html",],
   theme: {
     extend: {},
   },
   plugins: [
-
+    require('flowbite/plugin')
   ]
 }
